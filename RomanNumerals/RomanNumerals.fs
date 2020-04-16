57f14187d5cf49c2cfac90787c64bae982c94e8e
Matt Schouten <matt.schouten@hidigital.solutions>
2020-04-16 07:13:32 -0500

    Refactor to be data-driven

--- RomanNumerals/RomanNumerals.fs
+++ RomanNumerals/RomanNumerals.fs
@@ -2,31 +2,19 @@
 
 module Romanize = 
     let rec toRoman num =
-        let rec doOnes n = 
-            match n with
-            | 0 -> ""
-            | _ -> "I" + doOnes (n-1) 
+        let numerals = [(1000, "M"); (900, "CM"); (500, "D"); (400, "CD"); (100, "C"); 
+                        (90, "XC"); (50, "L"); (40, "XL"); (10, "X"); 
+                        (9, "IX"); (5, "V"); (4, "IV"); (1, "I")]
+        
+        let romanAppend n t =
+            let arabic = fst t
+            let roman = snd t
+            roman + toRoman (n-arabic)
 
         match num with
-        | n when n < 4 -> doOnes num
-        | 4 -> "IV"
-        | n when 5 <= n && n < 9 -> "V" + toRoman (n-5)
-        | 9 -> "IX"
-        | n when 10 <= n && n <= 39 -> "X" + toRoman (n-10)
-        | n when 40 <= n && n <= 49 -> "XL" + toRoman (n-40)
-        | n when 50 <= n && n <= 89 -> "L" + toRoman (n-50)
-        | n when 90 <= n && n <= 99 -> "XC" + toRoman (n-90)
-        | n when 100 <= n && n <= 399 -> "C" + toRoman (n-100)
-        | n when 400 <= n && n <= 499 -> "CD" + toRoman (n-400)
-        | n when 500 <= n && n <= 899 -> "D" + toRoman (n-500)
-        | n when 900 <= n && n <= 999 -> "CM" + toRoman (n-900)
-        | n when 1000 <= n -> "M" + toRoman(n-1000)
-        | _ -> "[shrug emoji]"
-
-        // Thinking there's a way to compress the ranges a bit...
-        // Also thinking there might be a way to tuple up the data
-        // Some rules:  Only I, X, C can be subtracted
-        // Only can subtract from the next two higher "digits"
-        // So I can be subtracted from V, X
-        // ...X can be subtracted from L, C
-        // ...C can be subtracted from D, M
+        | 0 -> ""
+        | _ -> numerals
+            |> List.tryFind( fun (arabic, roman) -> arabic <= num )
+            |> Option.get
+            |> romanAppend num
+        
