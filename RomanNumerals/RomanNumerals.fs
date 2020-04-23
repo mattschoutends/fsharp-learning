50b45985e593cd6655d4e6619120a8cf029fc4ca
Matt Schouten <matt.schouten@hidigital.solutions>
2020-04-23 07:12:53 -0500

    Worked through v1 of fsharpforfunandprofit Roman Numerals

--- RomanNumerals/RomanNumerals.fs
+++ RomanNumerals/RomanNumerals.fs
@@ -17,4 +17,100 @@ module Romanize =
             |> List.tryFind( fun (arabic, roman) -> arabic <= num )
             |> Option.get
             |> romanAppend num
-        
+
+module RomanTwo = 
+    // From F# for fun and profit, trying their Roman to Arabic example
+    // Doesn't use xUnit test cases, and I've been having trouble with 
+    // the discriminated union as a parameter to InlineData.
+    type RomanDigit = I | V | X | L | C | D | M
+    type RomanNumeral = RomanNumeral of RomanDigit list
+
+    let digitToInt = 
+        function
+        | I -> 1
+        | V -> 5
+        | X -> 10
+        | L -> 50
+        | C -> 100
+        | D -> 500
+        | M -> 1000
+
+    let rec digitsToInt =
+        function
+        | [] -> 0
+        | smaller::larger::ns when smaller < larger ->
+            (digitToInt larger - digitToInt smaller) + digitsToInt ns
+        | digit::ns -> digitToInt digit + digitsToInt ns 
+
+    let toInt (RomanNumeral digits) = digitsToInt digits
+
+    type ParsedChar =
+        | Digit of RomanDigit
+        | BadChar of char 
+
+    let charToRomanDigit = 
+        function
+        | 'I' -> Digit I
+        | 'V' -> Digit V
+        | 'X' -> Digit X
+        | 'L' -> Digit L
+        | 'C' -> Digit C
+        | 'D' -> Digit D
+        | 'M' -> Digit M
+        | ch -> BadChar ch
+
+    let toRomanDigitList (s:string) = 
+        s.ToCharArray()
+        |> List.ofArray
+        |> List.map charToRomanDigit
+
+    let toRomanNumeral s =
+        toRomanDigitList s
+        |> List.choose (
+            function
+            | Digit digit -> Some digit
+            | BadChar ch -> 
+                eprintf "%c is not a valid character" ch
+                None
+        )
+        |> RomanNumeral
+
+    let runsAllowed =
+        function
+        | I | X | C | M -> true
+        | V | L | D -> false
+
+    let noRunsAllowed = runsAllowed >> not
+
+    let rec isValidDigitList digitList =
+        match digitList with
+        | [] -> true
+        | d1::d2::d3::d4::d5::_
+            when d1=d2 && d1=d3 && d1=d4 && d1=d5 ->
+                false
+        | d1::d2::_
+            when d1=d2 && noRunsAllowed d1 ->
+                false 
+        | d1::d2::d3::d4::higher::ds
+            when d1=d2 && d1=d3 && d1=d4
+            && runsAllowed d1
+            && higher > d1 ->
+                false
+        | d1::d2::d3::higher::ds
+            when d1=d2 && d1=d3
+            && runsAllowed d1
+            && higher > d1 ->
+                false
+        | d1::d2::higher::ds
+            when d1=d2  
+            && runsAllowed d1
+            && higher > d1 ->
+                false
+        | d1::d2::d3::_
+            when d1 < d2 && d2 <= d3 ->
+                false
+        | _::ds ->
+            isValidDigitList ds
+
+    let isValid (RomanNumeral digitList) = 
+        isValidDigitList digitList
--- RomanNumeralsTest/Tests.fs
+++ RomanNumeralsTest/Tests.fs
@@ -109,3 +109,88 @@ let ``More than a thousand`` (roman: string, arabic: int) =
     Assert.Equal(roman, Romanize.toRoman arabic)
 
 
+open RomanNumerals.RomanTwo
+
+
+[<Fact>]
+let ``fsfp digit to int`` () =
+    Assert.Equal(I |> digitToInt, 1)
+    Assert.Equal(V |> digitToInt, 5)
+    Assert.Equal(M |> digitToInt, 1000)
+
+[<Fact>]
+let ``fsfp list of digits to int`` () =
+    Assert.Equal([I;I;I;I] |> digitsToInt, 4)
+    Assert.Equal([I;V] |> digitsToInt, 4)
+    Assert.Equal([V;I] |> digitsToInt, 6)
+    Assert.Equal([I;X] |> digitsToInt, 9)
+    Assert.Equal([M;C;M;L;X;X;I;X] |> digitsToInt, 1979)
+    Assert.Equal([M;C;M;X;L;I;V] |> digitsToInt, 1944)
+
+[<Fact>]
+let ``fsfp RomanNumeral to integer`` () =
+    Assert.Equal( RomanNumeral [I;I;I;I] |> toInt, 4)
+    Assert.Equal( RomanNumeral [M;C;M;L;X;X;I;X] |> toInt, 1979)
+
+[<Fact>]
+let ``fsfp toRomanNumeral`` () =
+    Assert.Equal("IIII" |> toRomanNumeral, RomanNumeral [I;I;I;I])
+    Assert.Equal("IV" |> toRomanNumeral, RomanNumeral [I;V])
+    Assert.Equal("VI" |> toRomanNumeral, RomanNumeral [V;I])
+    Assert.Equal("IX" |> toRomanNumeral, RomanNumeral [I;X])
+    Assert.Equal("MCMLXXIX" |> toRomanNumeral, RomanNumeral [M;C;M;L;X;X;I;X])
+    Assert.Equal("MCMXLIV" |> toRomanNumeral, RomanNumeral [M;C;M;X;L;I;V])
+    Assert.Equal("MC?I" |> toRomanNumeral, RomanNumeral [M;C;I])
+    Assert.Equal("abc" |> toRomanNumeral, RomanNumeral [])
+
+[<Fact>]
+let ``fsfp validation rules`` () = 
+    let validList = [
+        [I;I;I;I]
+        [I;V]
+        [I;X]
+        [I;X;V]
+        [X;I;V]
+        [X;I;X]
+        [X;X;I;I]
+    ]
+
+    validList
+    |> List.map (fun x -> Assert.True(isValidDigitList x, sprintf "%A" x))
+    |> ignore
+
+    let invalidList = [
+        [I;I;I;I;I]
+        [V;V]
+        [L;L]
+        [D;D]
+        [I;I;V]
+        [X;X;X;M]
+        [C;C;C;C;D]
+        [I;V;X]
+        [X;L;D]
+    ]
+
+    invalidList
+    |> List.map (fun x -> Assert.False(isValidDigitList x, sprintf "%A" x))
+
+[<Fact>]
+let ``fsfp top level isValid check`` () = 
+    Assert.True("IIII" |> toRomanNumeral |> isValid)
+    Assert.True("IV" |> toRomanNumeral |> isValid)
+    Assert.True("" |> toRomanNumeral |> isValid)
+
+    Assert.False("IIXX" |> toRomanNumeral |> isValid)
+    Assert.False("VV" |> toRomanNumeral |> isValid)
+
+[<Theory>]
+[<InlineData("IIII", true, 4)>]
+[<InlineData("XIV", true, 14)>]
+[<InlineData("MMDXC", true, 2590)>]
+[<InlineData("IIXX", false, 0)>]
+[<InlineData("VV", false, 0)>]
+let ``fsfp grand finale test`` (roman, valid, arabic) =
+    let romanNumeral = toRomanNumeral(roman)
+    Assert.Equal(isValid romanNumeral, valid)
+
+    if valid then Assert.Equal(toInt romanNumeral, arabic)
