2fc22b7b78815b1bfac243fbf645c188d1973294
Matt Schouten <matt.schouten@hidigital.solutions>
2020-04-24 07:02:09 -0500

    Parsing Roman numerals the second fsfp way

--- RomanNumerals/RomanNumerals.fs
+++ RomanNumerals/RomanNumerals.fs
@@ -114,3 +114,84 @@ module RomanTwo =
 
     let isValid (RomanNumeral digitList) = 
         isValidDigitList digitList
+
+module RomanThree =
+    type RomanDigit3 =
+        | I | II | III | IIII
+        | IV | V
+        | IX | X | XX | XXX | XXXX
+        | XL | L
+        | XC | C | CC | CCC | CCCC
+        | CD | D
+        | CM | M | MM | MMM | MMMM
+
+    let digitToInt3 = 
+        function
+        | I -> 1 | II -> 2 | III -> 3 | IIII -> 4
+        | IV -> 4 | V -> 5
+        | IX -> 9 | X -> 10 | XX -> 20 | XXX -> 30 | XXXX -> 40
+        | XL -> 40 | L -> 50
+        | XC -> 90 | C -> 100 | CC -> 200 | CCC -> 300 | CCCC -> 400
+        | CD -> 400 | D -> 500
+        | CM -> 900 | M -> 1000 | MM -> 2000 | MMM -> 3000 | MMMM -> 4000
+
+    let digitsToInt3 list = 
+        list |> List.sumBy digitToInt3
+
+    type RomanNumeral3 = RomanNumeral3 of RomanDigit3 list
+
+    let toInt3 (RomanNumeral3 digits) =
+        digitsToInt3 digits
+
+    type ParsedChar3 =
+        | Digit of RomanDigit3
+        | BadChar of char
+
+    let rec toRomanDigitListRec charList = 
+        match charList with
+        | 'I'::'I'::'I'::'I'::ns -> Digit IIII :: toRomanDigitListRec ns
+        | 'X'::'X'::'X'::'X'::ns -> Digit XXXX :: toRomanDigitListRec ns
+        | 'C'::'C'::'C'::'C'::ns -> Digit CCCC :: toRomanDigitListRec ns
+        | 'M'::'M'::'M'::'M'::ns -> Digit MMMM :: toRomanDigitListRec ns
+        | 'I'::'I'::'I'::ns -> Digit III :: toRomanDigitListRec ns
+        | 'X'::'X'::'X'::ns -> Digit XXX :: toRomanDigitListRec ns
+        | 'C'::'C'::'C'::ns -> Digit CCC :: toRomanDigitListRec ns
+        | 'M'::'M'::'M'::ns -> Digit MMM :: toRomanDigitListRec ns
+        | 'I'::'I'::ns -> Digit II :: toRomanDigitListRec ns
+        | 'X'::'X'::ns -> Digit XX :: toRomanDigitListRec ns
+        | 'C'::'C'::ns -> Digit CC :: toRomanDigitListRec ns
+        | 'M'::'M'::ns -> Digit MM :: toRomanDigitListRec ns
+
+        | 'I'::'V'::ns -> Digit IV :: toRomanDigitListRec ns
+        | 'I'::'X'::ns -> Digit IX :: toRomanDigitListRec ns
+        | 'X'::'L'::ns -> Digit XL :: toRomanDigitListRec ns
+        | 'X'::'C'::ns -> Digit XC :: toRomanDigitListRec ns
+        | 'C'::'D'::ns -> Digit CD :: toRomanDigitListRec ns
+        | 'C'::'M'::ns -> Digit CM :: toRomanDigitListRec ns
+
+        | 'I'::ns -> Digit I :: toRomanDigitListRec ns
+        | 'V'::ns -> Digit V :: toRomanDigitListRec ns
+        | 'X'::ns -> Digit X :: toRomanDigitListRec ns
+        | 'L'::ns -> Digit L :: toRomanDigitListRec ns
+        | 'C'::ns -> Digit C :: toRomanDigitListRec ns
+        | 'D'::ns -> Digit D :: toRomanDigitListRec ns
+        | 'M'::ns -> Digit M :: toRomanDigitListRec ns
+
+        | badChar::ns -> BadChar badChar :: toRomanDigitListRec ns
+        | [] -> []
+
+    let toRomanDigitList3 (s:string) = 
+        s.ToCharArray()
+        |> List.ofArray
+        |> toRomanDigitListRec
+    
+    let toRomanNumeral3 s =
+        toRomanDigitList3 s
+        |> List.choose(
+            function
+            | Digit digit -> Some digit
+            | BadChar ch -> 
+                eprintfn "%c is not a valid character" ch
+                None
+        )
+        |> RomanNumeral3
---
+++ RomanNumeralsTest/Tests3.fs
@@ -0,0 +1,38 @@
+module RomanNumeralsTests3
+
+open System
+open Xunit
+
+open RomanNumerals.RomanThree
+
+[<Fact>]
+let ``R3 digit to int`` () =
+    Assert.Equal(I |> digitToInt3, 1)
+    Assert.Equal(III |> digitToInt3, 3)
+    Assert.Equal(V |> digitToInt3, 5)
+    Assert.Equal(CM |> digitToInt3, 900)
+
+[<Fact>]
+let ``R3 list of digits to int`` () =
+    Assert.Equal([IIII] |> digitsToInt3, 4)
+    Assert.Equal([IV] |> digitsToInt3, 4)
+    Assert.Equal([V;I] |> digitsToInt3, 6)
+    Assert.Equal([IX] |> digitsToInt3, 9)
+    Assert.Equal([M;CM;L;X;X;IX] |> digitsToInt3, 1979)
+    Assert.Equal([M;CM;XL;IV] |> digitsToInt3, 1944)
+
+[<Fact>]
+let ``R3 RomanNumeral to integer`` () =
+    Assert.Equal( RomanNumeral3 [IIII] |> toInt3, 4)
+    Assert.Equal( RomanNumeral3 [M;CM;L;XX;IX] |> toInt3, 1979)
+
+[<Fact>]
+let ``R3 parsing`` () =
+    Assert.Equal("IIII" |> toRomanNumeral3, RomanNumeral3 [IIII])
+    Assert.Equal("IV" |> toRomanNumeral3, RomanNumeral3[IV])
+    Assert.Equal("VI" |> toRomanNumeral3, RomanNumeral3 [V;I])
+    Assert.Equal("IX" |> toRomanNumeral3, RomanNumeral3 [IX])
+    Assert.Equal("MCMLXXIX" |> toRomanNumeral3, RomanNumeral3 [M;CM;L;XX;IX])
+    Assert.Equal("MCMXLIV" |> toRomanNumeral3, RomanNumeral3 [M;CM;XL;IV])
+    Assert.Equal("MC?I" |> toRomanNumeral3, RomanNumeral3 [M;C;I])
+    Assert.Equal("abc" |> toRomanNumeral3, RomanNumeral3 [])
